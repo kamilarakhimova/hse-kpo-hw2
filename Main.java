0639f32871e0daaaa1f7a2b2213d5bb311347299
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2023-01-09 00:24:45 +0000

    add JavaDoc
    added a JavaDoc with describing of each method

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    /**
+     * This method runs the program
+     * @param args is not used, set by default
+     */
     public static void main(String[] args) {
         FileHandler.handleFiles();
     }
