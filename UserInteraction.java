385bc2a0363ed1ad227382018189deb9f34f0820
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2023-01-09 00:20:08 +0000

    add JavaDoc
    added a JavaDoc with describing of each method

--- UserInteraction.java
+++ UserInteraction.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class UserInteraction {
     public static String path;
     private static final String FORCED_PROGRAM_ENDING_INFO = "\nВ связи с этим, программа завершает свою работу.";
-    
+
     /**
-     * This method calls methods to print start info
+     * This method calls methods to print start information
      * @return is path to root directory
      */
     public static String startInfo() {
@@ -15,16 +15,27 @@ public class UserInteraction {
         return path;
     }
 
+    /**
+     * This method calls methods to print end information
+     */
     public static void endInfo() {
         printResultInfo();
         printGoodbye();
     }
 
+    /**
+     * This method prints that root directory was found
+     */
     public static void printFolderFound() {
         System.out.println("Ура, папка найдена. Приступаю к работе.");
         System.out.println();
     }
 
+    /**
+     * This method prints that file has wrong directive
+     * @param directive is wrong directive
+     * @param fileName is file that contains wrong directive
+     */
     public static void printWrongDirective(String directive, String fileName) {
         System.out.println("К сожалению, директива " + directive + " в файле " + fileName + " неверна." +
                 "\nСкорее всего, написание директивы некорректно, либо она ссылается на текущий или " +
@@ -32,12 +43,18 @@ public class UserInteraction {
         System.exit(0);
     }
 
+    /**
+     * This method prints that path to root directory is wrong
+     */
     public static void printWrongPath() {
         System.out.println("К сожалению, по указанному тобою пути не было найдено " +
                 "ни одного файла или папки." + FORCED_PROGRAM_ENDING_INFO);
         System.exit(0);
     }
 
+    /**
+     * This method prints that no txt files were found in root directory
+     */
     public static void printNoTxtFilesFound() {
         System.out.println("К сожалению, по указанному тобою пути не было найдено ни одного текстового " +
                 "файла." + FORCED_PROGRAM_ENDING_INFO);
@@ -46,20 +63,34 @@ public class UserInteraction {
         System.exit(0);
     }
 
+    /**
+     * This method prints that an error appeared while the file was read
+     */
     public static void printReadingError() {
         System.out.println("К сожалению, при чтении одного из файлов произошла ошибка." +
                 FORCED_PROGRAM_ENDING_INFO);
     }
 
+    /**
+     * This method prints that cycle dependencies were found
+     */
     public static void printCyclesFound() {
         System.out.println("К сожалению, были найдены циклические зависимости в следующих файлах:");
     }
 
+    /**
+     * This method prints that program forcibly shuts down
+     */
     public static void printEmergencyEnd() {
         System.out.println(FORCED_PROGRAM_ENDING_INFO);
         System.exit(0);
     }
 
+    /**
+     * This method prints that unknown error was found
+     * @param firstFileName is first file with unknown error
+     * @param secondFileName is second file with unknown error
+     */
     public static void printUnknownError(String firstFileName, String secondFileName) {
         System.out.println("К сожалению, в указанной тобой папке были найдены файлы " + firstFileName +
                 " и " + secondFileName + " с неопределёнными ошибками в директивах." +
@@ -67,6 +98,10 @@ public class UserInteraction {
         System.exit(0);
     }
 
+    /**
+     * This method prints paths to txt files in the right order (as stated in the condition)
+     * @param files is array of files in the right order
+     */
     public static void printSortedFiles(File[] files) {
         System.out.println("На всякий случай, если вдруг ты захочешь проверить корректность моих действий, " +
                 "вот получившийся отсортированный список файлов:");
@@ -76,25 +111,41 @@ public class UserInteraction {
         System.out.println();
     }
 
+    /**
+     * This method prints absolute path to result file
+     * @param rootPath is absolute path to root file
+     */
     public static void printResultPath(String rootPath) {
         System.out.println("Полный путь до файла с результатом работы: " + rootPath + "/result.txt");
     }
 
+    /**
+     * This method prints about appearance of an error with the result file
+     */
     public static void printResultError() {
         System.out.println("К сожалению, возникла ошибка, связанная с файлом, содержащим результат." +
                 FORCED_PROGRAM_ENDING_INFO);
     }
 
+    /**
+     * This method prints greeting
+     */
     private static void printGreeting() {
         System.out.println("Привет! Обработчик файлов готов к использованию.");
     }
 
+    /**
+     * This method sets path to root directory
+     */
     private static void setPath() {
         Scanner in = new Scanner(System.in);
         askDesireInput();
         getDesireInput(in.nextLine());
     }
 
+    /**
+     * This method asks user about a desire to personally enter the path to root directory
+     */
     private static void askDesireInput() {
         System.out.println("""
                 Ты хочешь ввести путь до входной папки самостоятельно?
@@ -105,6 +156,10 @@ public class UserInteraction {
                 Введи команду:""");
     }
 
+    /**
+     * This method gets and handles user's answer about a desire to personally enter the path to root directory
+     * @param answer is user's answer
+     */
     private static void getDesireInput(String answer) {
         switch (answer) {
             case "да", "yes", "+" -> askPath();
@@ -116,26 +171,41 @@ public class UserInteraction {
         }
     }
 
+    /**
+     * This method asks and gets path to root directory from user
+     */
     private static void askPath() {
         askInputPath();
         Scanner in = new Scanner(System.in);
         path = in.nextLine();
     }
 
+    /**
+     * This method asks user about path to root directory
+     */
     private static void askInputPath() {
         System.out.println("Введи путь до входной папки: ");
     }
 
+    /**
+     * This method sets default path to root directory
+     */
     private static void setDefaultPath() {
         path = "BasicExample";
     }
 
+    /**
+     * This method prints about incorrect input
+     */
     private static void printIncorrectInput() {
         System.out.println("""
                 К сожалению, введённая команда была некорректна :(
                 По умолчанию она обработается, как команда "нет".""");
     }
 
+    /**
+     * This method prints about getting the result
+     */
     private static void printResultInfo() {
         System.out.println("""
                 Всё готово! Результат находится в корневой папке в файле "result.txt".
@@ -143,6 +213,9 @@ public class UserInteraction {
                 """);
     }
 
+    /**
+     * This method prints goodbye
+     */
     private static void printGoodbye() {
         System.out.println("""
                 Надеюсь, я сумел тебе помочь.
@@ -150,3 +223,4 @@ public class UserInteraction {
                 Пока!""");
     }
 }
+                      
